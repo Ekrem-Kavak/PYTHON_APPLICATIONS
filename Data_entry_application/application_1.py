42d3d687d08a63ae7205deb1dfee5d3bfb4ea091
Ekrem Kavak <ekvk44@gmail.com>
2023-06-29 22:04:09 +0300

    Added menu and data entry functions.

--- Data_entry_application/application_1.py
+++ Data_entry_application/application_1.py
@@ -29,10 +29,88 @@ class data_input:
             self.exit()
 
     def menu(self):
-        pass
+        def control(select):
+            if re.search("[^1-4]",select):
+                raise Exception("Please choose a number between 1 and 4")
+            elif len(select)!= 1:
+                raise Exception("Please choose a number between 1 and 4")
+        while True:
+            try:
+                select = input("Hello, {} welcome. \n\n Please select the option you want to make.. \n\n [1]- Add data\n [2]- Delete data\n [3]- Read data\n [4]-Exit\n\n".format(self.programName))
+                control(select)
+            except Exception as exc:
+                print(exc)
+                time.sleep(3)
+            else:
+                break
+            return select
 
     def data_add(self):
-        pass
+        def controlName(name):
+            if name.isalpha() == False:
+                raise Exception("Entry only alphabetic characters.")
+        while True:
+            try:
+                name = input("Name: ")
+                controlName(name)
+            except Exception as exc_name:
+                print(exc_name)
+                time.sleep(3)
+            else:
+                break
+        
+        def controlSurname(surname):
+            if name.isalpha() == False:
+                raise Exception("Entry only alphabetic characters.")
+        while True:
+            try:
+                name = input("Surname: ")
+                controlSurname(surname)
+            except Exception as exc_surname:
+                print(exc_surname)
+                time.sleep(3)
+            else:
+                break
+
+        def controlAge(age):
+            if age.isdigit() == False:
+                raise Exception("Enter only numeric characters.")
+        while True:
+            try:
+                name = input("Age: ")
+                controlAge(age)
+            except Exception as exc_age:
+                print(exc_age)
+                time.sleep(3)
+            else:
+                break
+        
+        def controlID(identity):
+            if identity.digit() == False:
+                raise Exception("Enter only numeric characters.")
+        while True:
+            try: 
+                identity = input("Identity: ")
+                controlID(identity)
+            except Exception as exc_ID:
+                print(exc_ID)
+                time.sleep(3)
+            else:
+                break
+
+        def controlMail(Mail):
+            if not re.search("@" and ".com",Mail):
+                raise Exception("PLease enter a valid email address")
+        while True:
+            try:
+                Mail = input("Mail")
+                controlMail(Mail)
+            except Exception as exc_Mail:
+                print(exc_Mail)
+                time.sleep(3)
+            else:
+                break
+        
 
     def data_del(self):
         pass
