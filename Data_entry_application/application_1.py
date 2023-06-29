9be104237f23f76394c759cda6c545acba809e89
Ekrem Kavak <ekvk44@gmail.com>
2023-06-29 20:33:27 +0300

    The main functions of the system were created

--- Data_entry_application/application_1.py
+++ Data_entry_application/application_1.py
@@ -2,3 +2,36 @@ import re
 import time
 
 
+class data_input:
+    def __init__(self,programName):
+        self.programName = programName
+        self.loop = True
+
+
+    def program(self):
+        pass
+
+    def menu(self):
+        pass
+
+    def data_add(self):
+        pass
+
+    def data_del(self):
+        pass
+
+    def data_read(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def back_menu(self):
+        pass 
+
+
+
+
+system = data_input("Data Entering System")
+while system.loop:
+    system.program()
